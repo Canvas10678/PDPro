e9cc46f5a5b34448690c5eef14fd17370a0f5dd6
Upsornsri Ketchum <78495462+Canvas10678@users.noreply.github.com>
2021-04-26 13:44:28 +0700

    Add files via upload

---
+++ Pascal's Triangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+            do
+            {
+                Console.Write("Pascal's Triangle Size: ");
+                int ans = 1, b, u;
+                 Num = int.Parse(Console.ReadLine());
+                
+                if (Num < 0)
+                {
+                    Console.WriteLine("Invalid Pascal's triangle row number.");
+                    Console.WriteLine("Please enter again");
+                }
+                else 
+                {
+
+                    for (b = 0; b <= Num; b++)
+                    {
+                        for (u = 0; u <= b; u++)
+                        {
+                            if (u == 0 || b == 0)
+                                ans = 1;
+                            else
+                                ans = ans * (b - u + 1) / u;
+                            Console.Write(ans + " ");
+                        }
+                        Console.ReadLine();
+                    }
+                }
+            } while (Num < 0);
+
+        }
+    }
+}
+
+    
+
